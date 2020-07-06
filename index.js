16872d9536d2313929b1785812cb740bad8b2a81
nisticism <nistacular@gmail.com>
2020-07-06 02:40:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(names) {
+    const cards = []
+    for (let i = 0; i < names.length; i++) {
+
+        cards.push('Thank you, ' + names[i] + ', for the wonderful surprise gift!');
+    }
+    return cards;
+}
+
+function countDown(number) {
+    let i = 0;
+    while (i <= number) {
+      console.log(number - i);
+      i++;
+    }
+}
